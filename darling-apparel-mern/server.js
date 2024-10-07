7595ef9c2f840b31802a6340b9c6436d028b7aeb
samblygen <samblygen@yahoo.com>
2024-10-07 15:36:06 -0400

    creating backend server.js

---
+++ darling-apparel-mern/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(()=> console.log('MongoDB connected'))
+  .catch((err)=> console.log(err))
+
+
+app.get('/', (req,res)=>{
+  res.send('Welcome to Darling Apparel Boutique');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=> console.log(`Server running on ${PORT}`))
+
+
