35054c3e9e6b50119c67a193c297b1852fad29fb
samblygen <samblygen@yahoo.com>
2024-10-09 12:40:45 -0400

    order route update

--- darling-apparel-mern/server/routes/orderRoutes.js
+++ darling-apparel-mern/server/routes/orderRoutes.js
@@ -27,20 +27,39 @@ router.post('/', async(req,res)=>{
   });
 
   const savedOrder = await newOrder.save();
-  
+  res.status(201).json(savedOrder); 
 
 
  }catch (error){
-
+res.status(400).json({message: 'Error creating Order'})
  }
-})
+});
 
-router.patch ('/:id', (req, res)=>{
-  res.send(`Update order ${req.params.id}`);
-})
+router.patch ('/:id', async (req, res)=>{
+  const {id} = req.params;
+  try{
+const updatedOrder = await Order.findByIdAndUpdate(id, req.body, {new: true})
+if (!updatedOrder){
+  return res.status(404).json({message: 'order not found'})
+}
+res.json(updatedOrder);
 
-router.delete('/remove', (req,res)=>{
-  res.send(`Delete order ${req.params.id}`);
+  }catch(error){
+res.status(400).json({message: 'error updating order'})
+  }
+});
+
+router.delete('/:id',async (req,res)=>{
+  const {id} = req.params;
+  try{
+const deletedOrder = await Order.findByIdAndDelete(id);
+if (!deletedOrder){
+  return res.status(400).json({message: 'order not found'})
+}
+res.json({message: 'order deleted successfully'})
+  }catch (error){
+res.status(500).json({message: 'server error'})
+  }
 })
 
 export default router;
