1c7b0f40f3968a50c465a6e025bbfa178c459495
samblygen <samblygen@yahoo.com>
2024-10-09 12:55:13 -0400

    user registration post route

--- darling-apparel-mern/server/routes/userRoutes.js
+++ darling-apparel-mern/server/routes/userRoutes.js
@@ -2,19 +2,30 @@ import express from 'express'
 import User from '../models/User.js'
 const router = express.Router();
 
-router.post('/register', (req, res)=>{
-  res.send('Register user');
+router.post('/register', async (req, res)=>{
+ const {name, email, password} = req.body;
+
+ try{
+const existingUser = await User.findOne({email});
+if (existingUser){
+  return res.status(400).json({message: 'User exists'})
+}
+
+const hashedPassword = await bcrypt.hash(password, 10)
+ }catch{
+
+ }
 });
 
-router.post('/login', (req,res)=>{
+router.post('/login',async  (req,res)=>{
   res.send('User login');
 })
 
-router.patch ('/update', (req, res)=>{
+router.patch ('/update', async (req, res)=>{
   res.send('User update');
 })
 
-router.delete('/remove', (req,res)=>{
+router.delete('/remove', async (req,res)=>{
   res.send('Delete user');
 })
 
