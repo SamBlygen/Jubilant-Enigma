683cd3fc378531f186a7dd45db6a567c465ba57d
samblygen <samblygen@yahoo.com>
2024-10-14 12:26:14 -0400

    loginpage component setup

---
+++ darling-apparels-frontend/components/LoginPage.js
@@ -0,0 +1,35 @@
+export default function RegisterForm(){
+  const form = document.createElement('form');
+  form.id = 'registerForm';
+
+  form.innerHTML =`
+  <h2> Create an Account</h2>
+  <input type ="text" id="registerName" placeholder= "Name" required>
+  <input type ="email" id="registerEmail" placeholder= "Email" required>
+  <input type ="password" id="registerPassword" placeholder= "Password" required>
+  <button type ="submit">Register</button>
+  
+  `;
+
+  form.addEventListener('submit', async (e)=>{
+    e.preventDefault();
+    const name = document.getElementById('registerName').value;
+    const email = document.getElementById('registerEmail').value;
+    const password = document.getElementById('registerPassword').value;
+
+try{
+  const response = await fetch('http://localhost:5000/api/users/register',{
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({name, email, password}),
+  });
+  const data = await response.json();
+  alert(data.message || 'Registration failed');
+
+}catch(error) {
+alert('Error registering:' + error.message)
+}
+
+  })
+  return form;
+}
