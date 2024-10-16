3e10b58c6067e0a7da9ce12e7913702da102b5d7
samblygen <samblygen@yahoo.com>
2024-10-16 13:09:08 -0400

    updates to css and store front

--- darling-apparels-frontend/components/Background.jsx
+++ darling-apparels-frontend/components/Background.jsx
@@ -7,6 +7,7 @@ const Background = () => {
       <div className="background-overlay"></div>
       <div className="background-content">
         <h1 className='nice-animate'>Welcome to Our Store!</h1>
+        <p>Inspire. Create. Believe.</p>
         <p>“One is never over-dressed or under-dressed with a Little Black Dress.” - Karl Lagerfeld</p>
       </div>
     </div>
